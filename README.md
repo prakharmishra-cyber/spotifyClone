bb1d5d8bc02c218a10a6658268bb546972f7195e
Prakhar Mishra <mprakhar35@gmail.com>
2022-06-29 14:53:18 +0000

    1st commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# spotifyClone
-spotifyClone
---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import 'antd/dist/antd.less';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Hello World
+    </div>
+  );
+}
+
+export default App;
